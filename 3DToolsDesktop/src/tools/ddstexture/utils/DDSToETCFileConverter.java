5a15b26dd33120165933583c434acd5a65b25be8
Philip Jordan <p.j.nz@outlook.com>
2023-12-03 11:41:00 +1300

    thread factory for speed of processing

--- 3DToolsDesktop/src/tools/ddstexture/utils/DDSToETCFileConverter.java
+++ 3DToolsDesktop/src/tools/ddstexture/utils/DDSToETCFileConverter.java
@@ -11,6 +11,12 @@ import java.nio.Buffer;
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.prefs.Preferences;
 
 import javax.swing.JFrame;
@@ -43,122 +49,243 @@ import com.jogamp.newt.event.KeyEvent;
 import compressedtexture.DDSImage;
 import compressedtexture.KTXImage;
 import compressedtexture.dktxtools.ktx.KTXFormatException;
-import etcpack.QuickETC;
-import etcpack.ETCPack;
 import etcpack.ETCPack.FORMAT;
+import etcpack.QuickETC;
 import javaawt.VMEventQueue;
 import javaawt.image.VMBufferedImage;
 import javaawt.imageio.VMImageIO;
 import tools.swing.DetailsFileChooser;
 
 /**
- * dds image loading tester, note this use the decompressor to buffered image util system
- * not the jogl compressed call
+ * dds image loading tester, note this use the decompressor to buffered image util system not the jogl compressed call
  * @author philip
  *
  */
-public class DDSToETCFileConverter
-{
+public class DDSToETCFileConverter {
 	private static Preferences prefs;
-	
-	
-	public static void main(String[] args)
-	{
-		prefs = Preferences.userNodeForPackage(DDSToETCFileConverter.class);
-		
-		DetailsFileChooser dfc = new DetailsFileChooser(prefs.get("DDSToTexture", ""), new DetailsFileChooser.Listener() {
-			@Override
-			public void directorySelected(File dir)
-			{
-				prefs.put("DDSToTexture", dir.getParentFile().getAbsolutePath());
-				System.out.println("Selected dir: " + dir);
-				processDir(dir);
-			}
 
-			@Override
-			public void fileSelected(File file)
-			{
-				prefs.put("DDSToTexture", file.getAbsolutePath());
-				System.out.println("Selected file: " + file);
-				showImage(file, 15000);
-			}
-		});
+	public static void main(String[] args) {
+		prefs = Preferences.userNodeForPackage(DDSToETCFileConverter.class);
+
+		DetailsFileChooser dfc = new DetailsFileChooser(prefs.get("DDSToTexture", ""),
+				new DetailsFileChooser.Listener() {
+					@Override
+					public void directorySelected(File dir) {
+						prefs.put("DDSToTexture", dir.getParentFile().getAbsolutePath());
+						System.out.println("Selected dir: " + dir);
+						processDir(dir);
+					}
+
+					@Override
+					public void fileSelected(File file) {
+						prefs.put("DDSToTexture", file.getAbsolutePath());
+						System.out.println("Selected file: " + file);
+						processImage(file, 15000);
+					}
+				});
 
 		dfc.setFileFilter(new FileNameExtensionFilter("dds", "dds"));
 	}
 
-	private static void processDir(File dir)
-	{
+	
+	
+	
+	private static void processDir(File dir) {
 		System.out.println("Processing directory " + dir);
+		long startTime = System.currentTimeMillis();
 		File[] fs = dir.listFiles();
-		for (int i = 0; i < fs.length; i++)
-		{
-			try
-			{
-				if (fs[i].isFile() && fs[i].getName().endsWith(".dds"))
-				{
-					System.out.println("\tFile: " + fs[i]);
-					showImage(fs[i], 5000);
 
-					//pause between each show to gve it a chance to show
-					try
-					{
-						Thread.sleep(200);
-					}
-					catch (InterruptedException e)
-					{
-					}
-				}
-				else if (fs[i].isDirectory())
-				{
-					processDir(fs[i]);
-				}
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		List<Callable<Object>> todo = new ArrayList<Callable<Object>>();
 
-			}
-			catch (Exception ex)
-			{
+		// files then dirs
+		for (int i = 0; i < fs.length; i++) {
+			final int ii = i;
+			try {
+				if (fs[i].isFile() && fs[i].getName().endsWith(".dds")) {
+					todo.add(Executors.callable(new Runnable() {
+						@Override
+						public void run() {
+							//System.out.println("File: " + fs[ii]);
+							processImage(fs[ii], 0);
+						}
+					}));
+				}
+			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
+		
+		try {
+			List<Future<Object>> answers = es.invokeAll(todo);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		todo.clear();
+
+		for (int i = 0; i < fs.length; i++) {
+			try {
+				if (fs[i].isDirectory()) {
+					processDir(fs[i]);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		System.out.println(
+				"Finished processing directory " + dir + " in " + (System.currentTimeMillis() - startTime) + "ms");
 	}
 
-	public static void showImage(File file, long stayTime)
-	{
+	public static void processImage(File file, long stayTime) {
 		String filename = file.getAbsolutePath();
-		try
-		{
-			showImage(filename, new FileInputStream(file), stayTime);
-			
+		try {
+			convertImage(filename, new FileInputStream(file));
+			//showImage(filename, new FileInputStream(file), stayTime);
+
 			//TODO: this should be fine, but shows blank?
 			//showImageInShape(filename, new FileInputStream(file));
-		}
-		catch (IOException e)
-		{
-			System.out.println("" + DDSToETCFileConverter.class + " had a  IO problem with " + filename + " : " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println(
+					"" + DDSToETCFileConverter.class + " had a  IO problem with " + filename + " : " + e.getMessage());
 		}
 
 	}
 
-	public static void showImage(String filename, InputStream inputStream, final long stayTime)
-	{
+	public static void convertImage(String filename, InputStream inputStream) {
+		long tstart = System.currentTimeMillis();
+		DDSImage ddsImage;
+		try {
+			ddsImage = DDSImage.read(CompressedTextureLoader.toByteBuffer(inputStream));
+			//ddsImage.debugPrint();
+		} catch (IOException e) {
+			System.out.println(
+					"" + DDSToETCFileConverter.class + " had a  IO problem with " + filename + " : " + e.getMessage());
+			return;
+		}
+
+		if (ddsImage != null) {
+			DDSDecompressor decomp = new DDSDecompressor(ddsImage, 0, filename);
+			NioImageBuffer decompressedImage = decomp.convertImageNio();
+			Buffer b = decompressedImage.getDataBuffer();
+			if (b instanceof ByteBuffer) {
+				//ok so now find the RGB or RGBA byte buffers
+				ByteBuffer bb = (ByteBuffer)decompressedImage.getDataBuffer();
+				byte[] img = null;
+				byte[] imgalpha = null;
+				if (decompressedImage.getImageType() == NioImageBuffer.ImageType.TYPE_3BYTE_RGB) {
+					// just put the RGB data straight into the img byte array 
+					img = new byte[bb.capacity()];
+					bb.get(img, 0, bb.capacity());
+				} else if (decompressedImage.getImageType() == NioImageBuffer.ImageType.TYPE_4BYTE_RGBA) {
+					// copy RGB 3 sets out then 1 sets of alpha 
+					img = new byte[(bb.capacity() / 4) * 3];
+					imgalpha = new byte[(bb.capacity() / 4)];
+					for (int i = 0; i < img.length / 3; i++) {
+						img[i * 3 + 0] = bb.get();
+						img[i * 3 + 1] = bb.get();
+						img[i * 3 + 2] = bb.get();
+						imgalpha[i] = bb.get();
+					}
+				} else  if (decompressedImage.getImageType() == NioImageBuffer.ImageType.TYPE_BYTE_GRAY) {
+					// copy RGB from the 1 byte of L8 data and use RGB (FORMAT.ETC2PACKAGE_R is odd 16 bit thing)
+					img = new byte[bb.capacity() * 3];
+					for (int i = 0; i < img.length / 3; i++) {
+						byte byt = bb.get();
+						img[i * 3 + 0] = byt;
+						img[i * 3 + 1] = byt;
+						img[i * 3 + 2] = byt;
+					}
+				}else {
+					System.err.println("Bad Image Type " + decompressedImage.getImageType());
+					return;
+				}
+
+				//System.out.println("Debug of dds image " + filename);
+				//ddsImage.debugPrint();
+				int fmt = ddsImage.getPixelFormat();
+				FORMAT format = FORMAT.ETC2PACKAGE_RGBA;
+
+				if (fmt == DDSImage.D3DFMT_R8G8B8) {
+					format = FORMAT.ETC2PACKAGE_RGB;
+				} else if (fmt == DDSImage.D3DFMT_A8R8G8B8 || fmt == DDSImage.D3DFMT_X8R8G8B8) {
+					format = FORMAT.ETC2PACKAGE_RGBA;
+				} else if (fmt == DDSImage.D3DFMT_DXT1) {
+					if (!decomp.decompressedIsOpaque()) {
+						format = FORMAT.ETC2PACKAGE_sRGBA1;
+					} else {
+						format = FORMAT.ETC2PACKAGE_sRGB;
+					}
+				} else if (fmt == DDSImage.D3DFMT_DXT2	|| fmt == DDSImage.D3DFMT_DXT3 || fmt == DDSImage.D3DFMT_DXT4
+							|| fmt == DDSImage.D3DFMT_DXT5) {
+					if (!decomp.decompressedIsOpaque()) {
+						format = FORMAT.ETC2PACKAGE_sRGBA;
+					} else {
+						format = FORMAT.ETC2PACKAGE_sRGB;
+					}
+				} else if (fmt == DDSImage.D3DFMT_L8) {
+					format = FORMAT.ETC2PACKAGE_RGB;
+				}
+							 
+				
+				
+				//TODO, perhaps normal maps (_n) should be forcibly set to RGB ( not sRGBA)??
+				if(filename.indexOf("_n.dds") > 0)
+					format = FORMAT.ETC2PACKAGE_RGB;
+
+				ByteBuffer ktxBB = null;
+				QuickETC ep = new QuickETC();
+				ktxBB = ep.compressImageToByteBuffer(img, imgalpha, ddsImage.getWidth(), ddsImage.getHeight(), format,
+						false);
+				
+				String outfilename = filename.replace(".dds",".ktx");
+				outfilename = "D:\\temp\\" + outfilename.substring(outfilename.indexOf("game_media"));
+				File file = new File(outfilename);
+				file.getParentFile().mkdirs();
+				RandomAccessFile raf = null;
+				try {
+					raf = new RandomAccessFile(outfilename, "rw");
+					FileChannel fc = raf.getChannel();
+					ktxBB.rewind();
+					fc.write(ktxBB);
+					fc.close();
+					ktxBB.rewind();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} finally {
+					if (raf != null) {
+						try {
+							raf.close();
+						} catch (Exception e) {
+						}
+					}
+				}
+
+				System.out.println(""	+ (System.currentTimeMillis() - tstart) + "ms to compress " + filename + " to "
+									+ outfilename);
+			}
+		} else {
+			System.out.println("bum dds file for " + filename);
+		}
+	}
+
+	public static void showImage(String filename, InputStream inputStream, final long stayTime) {
 		// we are about to decompresed to buffered image below, we always need the VM support
 		javaawt.image.BufferedImage.installBufferedImageDelegate(VMBufferedImage.class);
 		javaawt.imageio.ImageIO.installBufferedImageImpl(VMImageIO.class);
-		javaawt.EventQueue.installEventQueueImpl(VMEventQueue.class);	
-		
+		javaawt.EventQueue.installEventQueueImpl(VMEventQueue.class);
+
 		final JFrame f = new JFrame();
 		f.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		f.getContentPane().setBackground(new Color(255, 0, 255));
 
 		DDSImage ddsImage;
-		try
-		{
+		try {
 			ddsImage = DDSImage.read(CompressedTextureLoader.toByteBuffer(inputStream));
 			//ddsImage.debugPrint();
-		}
-		catch (IOException e)
-		{
-			System.out.println("" + DDSToETCFileConverter.class + " had a  IO problem with " + filename + " : " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println(
+					"" + DDSToETCFileConverter.class + " had a  IO problem with " + filename + " : " + e.getMessage());
 			return;
 		}
 
@@ -177,16 +304,14 @@ public class DDSToETCFileConverter
 				img = new byte[bb.capacity()];
 				bb.get(img, 0, bb.capacity());
 			} else if (decompressedImage.getImageType() == NioImageBuffer.ImageType.TYPE_4BYTE_RGBA) {
-				byte[] ddsimg = new byte[bb.capacity()];
-				bb.get(ddsimg, 0, bb.capacity());
 				// copy RGB 3 sets out then 1 sets of alpha 
 				img = new byte[(bb.capacity() / 4) * 3];
 				imgalpha = new byte[(bb.capacity() / 4)];
 				for (int i = 0; i < img.length / 3; i++) {
-					img [i * 3 + 0] = ddsimg [i * 4 + 0];
-					img [i * 3 + 1] = ddsimg [i * 4 + 1];
-					img [i * 3 + 2] = ddsimg [i * 4 + 2];
-					imgalpha [i] = ddsimg [i * 4 + 3];
+					img[i * 3 + 0] = bb.get();
+					img[i * 3 + 1] = bb.get();
+					img[i * 3 + 2] = bb.get();
+					imgalpha[i] = bb.get();
 				}
 			} else {
 				System.err.println("Bad Image Type " + decompressedImage.getImageType());
@@ -196,35 +321,33 @@ public class DDSToETCFileConverter
 			//System.out.println("Debug of dds image " + filename);
 			//ddsImage.debugPrint();
 			int fmt = ddsImage.getPixelFormat();
-			FORMAT format = FORMAT.ETC2PACKAGE_RGBA; 
-									
+			FORMAT format = FORMAT.ETC2PACKAGE_RGBA;
+
 			if (fmt == DDSImage.D3DFMT_R8G8B8) {
-				 format = FORMAT.ETC2PACKAGE_RGB;
+				format = FORMAT.ETC2PACKAGE_RGB;
 			} else if (fmt == DDSImage.D3DFMT_A8R8G8B8 || fmt == DDSImage.D3DFMT_X8R8G8B8) {
 				format = FORMAT.ETC2PACKAGE_RGBA;
 			} else if (fmt == DDSImage.D3DFMT_DXT1) {
-				if(!decomp.decompressedIsOpaque()) {
+				if (!decomp.decompressedIsOpaque()) {
 					format = FORMAT.ETC2PACKAGE_sRGBA1;
 				} else {
 					format = FORMAT.ETC2PACKAGE_sRGB;
 				}
-			} else if (fmt == DDSImage.D3DFMT_DXT2 || fmt == DDSImage.D3DFMT_DXT3
-					||fmt == DDSImage.D3DFMT_DXT4 || fmt == DDSImage.D3DFMT_DXT5) {
-				if(!decomp.decompressedIsOpaque()) {
+			} else if (fmt == DDSImage.D3DFMT_DXT2	|| fmt == DDSImage.D3DFMT_DXT3 || fmt == DDSImage.D3DFMT_DXT4
+						|| fmt == DDSImage.D3DFMT_DXT5) {
+				if (!decomp.decompressedIsOpaque()) {
 					format = FORMAT.ETC2PACKAGE_sRGBA;
 				} else {
 					format = FORMAT.ETC2PACKAGE_sRGB;
 				}
-			}	
-			
+			}
+
 			ByteBuffer ktxBB = null;
 			try {
-				
-				System.out.println("DDSDecompressor.opaque " +decomp.decompressedIsOpaque());
-				//TODO: rip through and check for no non255 alpha and make it a non A type
 				QuickETC ep = new QuickETC();
 				//ETCPack ep = new ETCPack();
-				ktxBB = ep.compressImageToByteBuffer(img, imgalpha, ddsImage.getWidth(), ddsImage.getHeight(), format, false);									
+				ktxBB = ep.compressImageToByteBuffer(img, imgalpha, ddsImage.getWidth(), ddsImage.getHeight(), format,
+						false);
 				KTXImage ktxImage = new KTXImage(ktxBB);
 			} catch (KTXFormatException e) {
 				System.out.println("DDS to KTX image: " + filename);
@@ -232,36 +355,16 @@ public class DDSToETCFileConverter
 			} catch (IOException e) {
 				System.out.println("DDS to KTX image: " + filename);
 				e.printStackTrace();
-			} catch(BufferOverflowException e) {
+			} catch (BufferOverflowException e) {
 				System.out.println("DDS to KTX image: " + filename);
 				e.printStackTrace();
 			}
-			
-			
-			
-			
-			filename = "D:\\temp\\"+filename.substring(filename.indexOf("textures"))+".ktx";
-			File file = new File(filename);
-			file.getParentFile().mkdirs();
-			try {
-				//yeeha! lets write this bad boy out to a file!!!
-				RandomAccessFile raf = new RandomAccessFile(filename, "rw");
-				
-				FileChannel fc = raf.getChannel();
-				ktxBB.rewind();
-				fc.write(ktxBB);
-				fc.close();
-				ktxBB.rewind();
-			} catch (IOException e1) {
-		 
-				e1.printStackTrace();
-			}
+
 		}
 	}
 
-	//Method to show teh DXT texture method in a 3d scene
-	public static void showImageInShape(String filename, InputStream inputStream)
-	{
+	//Method to show teh ETC texture method in a 3d scene
+	public static void showImageInShape(String filename, InputStream inputStream) {
 		//note win construction MUST occur beofre asking for graphics environment etc.
 		//JFrame win = new JFrame("Fullscreen Example");
 		//win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -269,15 +372,13 @@ public class DDSToETCFileConverter
 		Canvas3D canvas3D = new Canvas3D();
 		//win.add(canvas3D);
 		canvas3D.addNotify();
-		
+
 		//GraphicsSettings gs = ScreenResolution.organiseResolution(null, win, false, true, true);
 
 		canvas3D.getGLWindow().addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e)
-			{
+			public void keyPressed(KeyEvent e) {
 				final int keyCode = e.getKeyCode();
-				if ((keyCode == KeyEvent.VK_ESCAPE) || ((keyCode == KeyEvent.VK_C) && e.isControlDown()))
-				{
+				if ((keyCode == KeyEvent.VK_ESCAPE) || ((keyCode == KeyEvent.VK_C) && e.isControlDown())) {
 					System.exit(0);
 				}
 			}
@@ -300,29 +401,29 @@ public class DDSToETCFileConverter
 	 * Builds a scenegraph for the application to render.
 	 * @return the root level of the scenegraph
 	 */
-	private static BranchGroup createSceneGraph(String filename, InputStream inputStream)
-	{
+	private static BranchGroup createSceneGraph(String filename, InputStream inputStream) {
 		final BranchGroup objRoot = new BranchGroup();
 
 		Shape3D shape = new Shape3D();
-		IndexedTriangleStripArray tri = new IndexedTriangleStripArray(4, GeometryArray.USE_COORD_INDEX_ONLY | GeometryArray.COORDINATES | GeometryArray.TEXTURE_COORDINATE_2, 6, new int[] {6});
+		IndexedTriangleStripArray tri = new IndexedTriangleStripArray(4,
+				GeometryArray.USE_COORD_INDEX_ONLY | GeometryArray.COORDINATES | GeometryArray.TEXTURE_COORDINATE_2, 6,
+				new int[] {6});
 		tri.setCoordinate(0, new Point3f(-0.5f, 0.0f, 0.0f));
 		tri.setCoordinate(1, new Point3f(0.5f, 0.0f, 0.0f));
 		tri.setCoordinate(2, new Point3f(0.5f, 0.5f, 0.0f));
 		tri.setCoordinate(3, new Point3f(-0.5f, 0.5f, 0.0f));
-		
+
 		tri.setTextureCoordinate(0, 0, new TexCoord2f(0.0f, 0.0f));
 		tri.setTextureCoordinate(0, 1, new TexCoord2f(1.0f, 0.0f));
 		tri.setTextureCoordinate(0, 2, new TexCoord2f(1.0f, 1.0f));
 		tri.setTextureCoordinate(0, 3, new TexCoord2f(0.0f, 1.0f));
-		
+
 		tri.setCoordinateIndex(0, 0);
 		tri.setCoordinateIndex(0, 1);
 		tri.setCoordinateIndex(0, 2);
 		tri.setCoordinateIndex(0, 0);
 		tri.setCoordinateIndex(0, 3);
 		tri.setCoordinateIndex(0, 2);
-		
 
 		// Because we're about to spin this triangle, be sure to draw
 		// backfaces.  If we don't, the back side of the triangle is invisible.
@@ -340,7 +441,7 @@ public class DDSToETCFileConverter
 		Transform3D yAxis = new Transform3D();
 		Alpha rotationAlpha = new Alpha(-1, 4000);
 		tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-		RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, tg, yAxis, 0.0f, (float) Math.PI * 2.0f);
+		RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, tg, yAxis, 0.0f, (float)Math.PI * 2.0f);
 		rotator.setSchedulingBounds(bounds);
 
 		shape.setGeometry(tri);
